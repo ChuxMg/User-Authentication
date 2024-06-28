a70d98f6d7401d4f7ae738ee030e658c8e2cc8c4
ChuxMg <chuxmgbojikwe@gmail.com>
2024-06-28 07:16:15 +0700

    Authentication Error Handling

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,17 +1,24 @@
-import { useState } from 'react';
-import AuthContent from '../components/Auth/AuthContent';
-import LoadingOverlay from '../components/ui/LoadingOverlay';
-import { login } from '../Util/auth';
+import { useState } from "react";
+import AuthContent from "../components/Auth/AuthContent";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { login } from "../Util/auth";
+import { Alert } from "react-native";
 
 function LoginScreen() {
   const [isAuthenticating, setIsAuthenticating] = useState(false);
 
   async function loginHandler({ email, password }) {
     setIsAuthenticating(true);
-    await login(email, password);
+    try {
+      await login(email, password);
+    } catch (error) {
+      Alert.alert(
+        "Authentication failed!",
+        "Could not log you in. Please check your credentials or try again later!"
+      );
+    }
     setIsAuthenticating(false);
   }
-
 
   if (isAuthenticating) {
     return <LoadingOverlay message="Logging you in..." />;
--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,23 +1,28 @@
-import { useState } from 'react';
-import AuthContent from '../components/Auth/AuthContent';
-import { createUser } from '../Util/auth';
-import LoadingOverlay from '../components/ui/LoadingOverlay';
+import { useState } from "react";
+import AuthContent from "../components/Auth/AuthContent";
+import { createUser } from "../Util/auth";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
 
 function SignupScreen() {
-
   const [isAuthenticating, setIsAuthenticating] = useState(false);
 
-  async function signupHandler({email, password}) {
+  async function signupHandler({ email, password }) {
     setIsAuthenticating(true);
-    await createUser(email, password)
+    try {
+      await createUser(email, password);
+    } catch (error) {
+      Alert.alert(
+        "Authentication failed!",
+        "Could not create user. Please check your input and try again later."
+      );
+    }
     setIsAuthenticating(false);
   }
 
   if (isAuthenticating) {
-    return <LoadingOverlay message="Creating user..." />
+    return <LoadingOverlay message="Creating user..." />;
   }
-
-
+  
   return <AuthContent onAuthenticate={signupHandler} />;
 }
 
