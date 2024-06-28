1e4906297df05d5bb430b73a13dad4e05375c5ad
ChuxMg <chuxmgbojikwe@gmail.com>
2024-06-28 08:36:11 +0700

    Accessing Protected Resources

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 function WelcomeScreen() {
+  const [fetchedMessage, setFetchedMessage] = useState("");
+  useEffect(() => {
+    axios
+      .get(
+        "https://react-native-course-data-default-rtdb.firebaseio.com/message.json"
+      )
+      .then((response) => {
+        setFetchedMessage(response.data);
+      });
+  }, []);
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
     </View>
   );
 }
@@ -14,13 +27,13 @@ export default WelcomeScreen;
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     padding: 32,
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 8,
   },
 });
